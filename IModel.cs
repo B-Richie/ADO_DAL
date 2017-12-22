def90eae7107cda39440a8851388125929723105
dicRichie <dicRichie@users.noreply.github.com>
2017-12-22 15:15:44 -0500

    Add files via upload

---
+++ IModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AutomobileApp.Entities
+{
+    public interface IModel
+    {
+        void Delete(int id);
+        Object Insert(Model model);
+        Model MapModelObject(SqlDataReader dr);
+        List<Model> GetModelsByMakerId(int Id);
+
+    }
+}
